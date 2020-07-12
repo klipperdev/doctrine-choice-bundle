c243f29565f924d8cc15d9c265355d8de78df570
François Pluchino <francois.pluchino@klipper.dev>
2020-07-12 12:42:58 +0200

    Add data loader and init command to load doctrine choices

---
+++ DependencyInjection/KlipperDoctrineChoiceExtension.php
@@ -0,0 +1,36 @@
+<?php
+
+/*
+ * This file is part of the Klipper package.
+ *
+ * (c) François Pluchino <francois.pluchino@klipper.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Klipper\Bundle\DoctrineChoiceBundle\DependencyInjection;
+
+use Klipper\Component\DataLoader\DataLoaderInterface;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+
+/**
+ * @author François Pluchino <francois.pluchino@klipper.dev>
+ */
+class KlipperDoctrineChoiceExtension extends Extension
+{
+    /**
+     * @throws
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+
+        if (interface_exists(DataLoaderInterface::class)) {
+            $loader->load('data_loader_command.xml');
+        }
+    }
+}
